eafd8e65bb29676cc937bf5c84dbd117f33b1977
mukksre <55061315+mukksre@users.noreply.github.com>
2022-08-17 01:22:50 -0500

    Update ListAllView.java

--- src/main/java/com/example/application/views/listall/ListAllView.java
+++ src/main/java/com/example/application/views/listall/ListAllView.java
@@ -5,11 +5,14 @@ import com.example.application.service.RegistrationService;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 import java.util.Base64;
 import java.util.List;
 
@@ -23,35 +26,42 @@ public class ListAllView extends HorizontalLayout {
         "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=";
 
     public ListAllView() {
-        setWidth(100, Unit.PERCENTAGE);
-        setHeight(100, Unit.PERCENTAGE);
-        VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.setWidth(100, Unit.PERCENTAGE);
-        verticalLayout.setHeight(100, Unit.PERCENTAGE);
-        add(verticalLayout);
-        verticalLayout.add(new Label("Patient Registration List"));
-        Grid<Patient> grid = new Grid<>();
-        List<Patient> allPatients = registrationService.getAllPatients();
-        grid.setItems(allPatients);
-        grid.addColumn(Patient::getId).setHeader("ID").setResizable(true);
-        grid.addColumn(Patient::getName).setHeader("Name").setResizable(true).setWidth("100px");
-        grid.addColumn(Patient::getDateOfBirth).setHeader("Date of birth");
-        grid.addColumn(Patient::getEmail).setHeader("Email").setResizable(true).setWidth("100px");
-        grid.addColumn(Patient::getAddressLine1).setHeader("Address Line 1").setResizable(true).setWidth("100px");
-        grid.addColumn(Patient::getAddressLine2).setHeader("Address Line 2").setResizable(true).setWidth("100px");
-        grid.addColumn((Patient::getCity)).setHeader("City").setResizable(true);
-        grid.addColumn(Patient::getState).setHeader("State").setResizable(true);
-        grid.addColumn(Patient::getZipCode).setHeader("Zip Code").setResizable(true);
-        grid.addColumn(Patient::getAppointmentTime).setHeader("Appointment Time").setResizable(true).setWidth("150px");
-        grid.addColumn(
-            TemplateRenderer
-                .<Patient>of(
-                    "<div><img style='height: 80px; width: 80px;' src='[[item.imageBytes]]' alt='[[item.name]]'/></div>"
-                )
-                .withProperty("imageBytes", item -> getImageAsBase64(item.getImageBytes()))
-                .withProperty("name", item -> item.getName())
-        ).setHeader("Driver License");
-        verticalLayout.add(grid);
+        String user = (String)VaadinSession.getCurrent().getAttribute("user");
+        if(user.equals("patient")) {
+            Notification inPermissibleNotification =
+                Notification.show("You are not permitted to see this", 3000, Notification.Position.MIDDLE);
+            inPermissibleNotification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+        }else if (user.equals("admin")){
+            setWidth(100, Unit.PERCENTAGE);
+            setHeight(100, Unit.PERCENTAGE);
+            VerticalLayout verticalLayout = new VerticalLayout();
+            verticalLayout.setWidth(100, Unit.PERCENTAGE);
+            verticalLayout.setHeight(100, Unit.PERCENTAGE);
+            add(verticalLayout);
+            verticalLayout.add(new Label("Patient Registration List"));
+            Grid<Patient> grid = new Grid<>();
+            List<Patient> allPatients = registrationService.getAllPatients();
+            grid.setItems(allPatients);
+            grid.addColumn(Patient::getId).setHeader("ID").setResizable(true);
+            grid.addColumn(Patient::getName).setHeader("Name").setResizable(true).setWidth("100px");
+            grid.addColumn(Patient::getDateOfBirth).setHeader("Date of birth");
+            grid.addColumn(Patient::getEmail).setHeader("Email").setResizable(true).setWidth("100px");
+            grid.addColumn(Patient::getAddressLine1).setHeader("Address Line 1").setResizable(true).setWidth("100px");
+            grid.addColumn(Patient::getAddressLine2).setHeader("Address Line 2").setResizable(true).setWidth("100px");
+            grid.addColumn((Patient::getCity)).setHeader("City").setResizable(true);
+            grid.addColumn(Patient::getState).setHeader("State").setResizable(true);
+            grid.addColumn(Patient::getZipCode).setHeader("Zip Code").setResizable(true);
+            grid.addColumn(Patient::getAppointmentTime).setHeader("Appointment Time").setResizable(true).setWidth("150px");
+            grid.addColumn(
+                TemplateRenderer
+                    .<Patient>of(
+                        "<div><img style='height: 80px; width: 80px;' src='[[item.imageBytes]]' alt='[[item.name]]'/></div>"
+                    )
+                    .withProperty("imageBytes", item -> getImageAsBase64(item.getImageBytes()))
+                    .withProperty("name", item -> item.getName())
+            ).setHeader("Driver License");
+            verticalLayout.add(grid);
+        }
     }
 
     private String getImageAsBase64(byte[] string) {
